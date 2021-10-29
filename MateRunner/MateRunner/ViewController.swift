a7b88563db8607f738c503e0d275cc6f96ef03e7
jungwonlee <dev.jungwon.lee@gmail.com>
2021-10-29 16:45:24 +0900

    [Chore] 초기 프로젝트 설정

---
+++ MateRunner/MateRunner/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  MateRunner
+//
+//  Created by 이정원 on 2021/10/29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
