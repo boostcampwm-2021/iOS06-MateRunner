95f2a913285826a69dabbb0cfb0324a0a84f2570
jungwonlee <dev.jungwon.lee@gmail.com>
2021-11-01 15:19:05 +0900

    [Refactor] Lint 적용

--- MateRunner/MateRunner/Scene/Common/ViewController/TabBarController.swift
+++ MateRunner/MateRunner/Scene/Common/ViewController/TabBarController.swift
@@ -10,28 +10,44 @@ import UIKit
 final class TabBarController: UITabBarController {
     private lazy var homeNavigationController: UINavigationController = {
         let homeNavigationController = UINavigationController(rootViewController: HomeViewController())
-        homeNavigationController.tabBarItem = UITabBarItem(title: nil, image: UIImage(named: "home"), selectedImage: nil)
+        homeNavigationController.tabBarItem = UITabBarItem(
+            title: nil,
+            image: UIImage(named: "home"),
+            selectedImage: nil
+        )
         homeNavigationController.tabBarItem.imageInsets = UIEdgeInsets(top: 5, left: 0, bottom: 0, right: 0)
         return homeNavigationController
     }()
 
     private lazy var recordNavigationController: UINavigationController = {
         let recordNavigationController = UINavigationController(rootViewController: RecordViewController())
-        recordNavigationController.tabBarItem = UITabBarItem(title: nil, image: UIImage(named: "record"), selectedImage: nil)
+        recordNavigationController.tabBarItem = UITabBarItem(
+            title: nil,
+            image: UIImage(named: "record"),
+            selectedImage: nil
+        )
         recordNavigationController.tabBarItem.imageInsets = UIEdgeInsets(top: 5, left: 0, bottom: 0, right: 0)
         return recordNavigationController
     }()
 
     private lazy var mateNavigationController: UINavigationController = {
         let mateNavigationController = UINavigationController(rootViewController: MateViewController())
-        mateNavigationController.tabBarItem = UITabBarItem(title: nil, image: UIImage(named: "mate"), selectedImage: nil)
+        mateNavigationController.tabBarItem = UITabBarItem(
+            title: nil,
+            image: UIImage(named: "mate"),
+            selectedImage: nil
+        )
         mateNavigationController.tabBarItem.imageInsets = UIEdgeInsets(top: 5, left: 0, bottom: 0, right: 0)
         return mateNavigationController
     }()
 
     private lazy var myPageNavigationController: UINavigationController = {
         let myPageNavigationController = UINavigationController(rootViewController: MyPageViewController())
-        myPageNavigationController.tabBarItem = UITabBarItem(title: nil, image: UIImage(named: "mypage"), selectedImage: nil)
+        myPageNavigationController.tabBarItem = UITabBarItem(
+            title: nil,
+            image: UIImage(named: "mypage"),
+            selectedImage: nil
+        )
         myPageNavigationController.tabBarItem.imageInsets = UIEdgeInsets(top: 5, left: 0, bottom: 0, right: 0)
         return myPageNavigationController
     }()
@@ -41,6 +57,8 @@ final class TabBarController: UITabBarController {
         self.configure()
     }
 }
+
+// MARK: - Private Functions
 
 private extension TabBarController {
     func configure() {
