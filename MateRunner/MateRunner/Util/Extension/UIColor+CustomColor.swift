4b9f366136baae4ca290eb72650b3f2ba827548c
jungwonlee <dev.jungwon.lee@gmail.com>
2021-11-01 15:03:36 +0900

    [Feat] Custom Color 추가

--- MateRunner/MateRunner/Util/Extension/UIColor+CustomColor.swift
+++ MateRunner/MateRunner/Util/Extension/UIColor+CustomColor.swift
@@ -5,4 +5,14 @@
 //  Created by 이정원 on 2021/11/01.
 //
 
-import Foundation
+import UIKit
+
+extension UIColor {
+    static var mrPurple: UIColor {
+        return UIColor(red: 122.0 / 255.0, green: 126.0 / 255.0, blue: 247.0 / 255.0, alpha: 1)
+    }
+
+    static var mrYellow: UIColor {
+        return UIColor(red: 255.0 / 255.0, green: 233.0 / 255.0, blue: 129.0 / 255.0, alpha: 1)
+    }
+}
