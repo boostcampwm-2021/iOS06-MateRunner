5c24b8d5091b1f3dd9d6835d14e24fcd427455ce
lee-yujinn <leeyj7988@naver.com>
2021-11-02 18:36:52 +0900

    [Feat] UIView shadow extension 추가

--- MateRunner/MateRunner/Util/Extension/UIView+ShadowEffect.swift
+++ MateRunner/MateRunner/Util/Extension/UIView+ShadowEffect.swift
@@ -5,4 +5,31 @@
 //  Created by 이유진 on 2021/11/02.
 //
 
-import Foundation
+import UIKit
+
+extension UIView {
+    enum Shadow {
+        case bottom, top, left, right
+    }
+
+    func addShadow(location: Shadow, color: UIColor = .systemGray4, opacity: Float = 0.8, radius: CGFloat = 5.0) {
+        switch location {
+        case .bottom:
+            addShadow(offset: CGSize(width: 0, height: 10), color: color, opacity: opacity, radius: radius)
+        case .top:
+            addShadow(offset: CGSize(width: 0, height: -10), color: color, opacity: opacity, radius: radius)
+        case .left:
+            addShadow(offset: CGSize(width: -10, height: 0), color: color, opacity: opacity, radius: radius)
+        case .right:
+            addShadow(offset: CGSize(width: 10, height: 0), color: color, opacity: opacity, radius: radius)
+        }
+    }
+
+    func addShadow(offset: CGSize, color: UIColor = .black, opacity: Float = 0.1, radius: CGFloat = 3.0) {
+        self.layer.masksToBounds = false
+        self.layer.shadowColor = color.cgColor
+        self.layer.shadowOffset = offset
+        self.layer.shadowOpacity = opacity
+        self.layer.shadowRadius = radius
+    }
+}
