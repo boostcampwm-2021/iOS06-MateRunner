5ba718bc9c7136eadf107c82232f84cbf40d1eab
jungwonlee <dev.jungwon.lee@gmail.com>
2021-11-02 13:10:13 +0900

    [Feat] Extension에 Italic체 computed property 추가

--- MateRunner/MateRunner/Util/Extension/UIFont+CustomFont.swift
+++ MateRunner/MateRunner/Util/Extension/UIFont+CustomFont.swift
@@ -15,4 +15,20 @@ extension UIFont {
     static func notoSans(size: CGFloat = 10, family: Family = .regular) -> UIFont {
         return UIFont(name: "NotoSansKR-\(family)", size: size) ?? UIFont.systemFont(ofSize: size)
     }
+    
+    var bold: UIFont {
+        return with(.traitBold)
+    }
+
+    var italic: UIFont {
+        return with(.traitItalic)
+    }
+    
+    private func with(_ traits: UIFontDescriptor.SymbolicTraits...) -> UIFont {
+        let symbolicTraits = UIFontDescriptor.SymbolicTraits(traits).union(self.fontDescriptor.symbolicTraits)
+        guard let descriptor = self.fontDescriptor.withSymbolicTraits(symbolicTraits) else {
+            return self
+        }
+        return UIFont(descriptor: descriptor, size: 0)
+    }
 }
