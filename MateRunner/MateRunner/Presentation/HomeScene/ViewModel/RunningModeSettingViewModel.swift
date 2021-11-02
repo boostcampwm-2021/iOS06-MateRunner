102e8757b62624b3e3fd2d9deea1606911d8764b
lee-yujinn <leeyj7988@naver.com>
2021-11-02 18:38:11 +0900

    [Feat] RunningSettingViewModel 추가 구조개선

--- MateRunner/MateRunner/Presentation/HomeScene/ViewModel/RunningModeSettingViewModel.swift
+++ MateRunner/MateRunner/Presentation/HomeScene/ViewModel/RunningModeSettingViewModel.swift
@@ -6,3 +6,56 @@
 //
 
 import Foundation
+import RxSwift
+import RxCocoa
+
+@propertyWrapper
+public struct BehaviorRelayProperty<Value> {
+    private var subject: BehaviorRelay<Value>
+    public var wrappedValue: Value {
+        get { subject.value }
+        set { subject.accept(newValue) }
+    }
+    
+    public var projectedValue: BehaviorRelay<Value> {
+        return self.subject
+    }
+    
+    public init(wrappedValue: Value) {
+        subject = BehaviorRelay(value: wrappedValue)
+    }
+}
+
+enum RunningMode {
+    case single, mate
+}
+
+final class RunningModeSettingViewModel {
+    struct Input {
+        let singleButtonTapEvent: Driver<Void>
+        let mateButtonTapEvent: Driver<Void>
+    }
+    
+    struct Output {
+        @BehaviorRelayProperty var runningMode: RunningMode?
+    }
+    
+    func transform(from input: Input, disposeBag: DisposeBag) -> Output {
+        var output = Output()
+        input.singleButtonTapEvent
+            .do(onNext: {
+                output.runningMode = RunningMode.single
+            })
+            .drive()
+            .disposed(by: disposeBag)
+
+        input.mateButtonTapEvent
+            .do(onNext: {
+                output.runningMode = RunningMode.mate
+            })
+            .drive()
+            .disposed(by: disposeBag)
+
+        return output
+    }
+}
