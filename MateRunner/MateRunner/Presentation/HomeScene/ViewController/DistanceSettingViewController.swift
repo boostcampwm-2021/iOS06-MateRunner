3d90e4f36a19c7914f00da5f10f781dfb73a85c3
jungwonlee <dev.jungwon.lee@gmail.com>
2021-11-02 13:12:30 +0900

    [Feat] 목표거리 설정 화면 UI 구현

--- MateRunner/MateRunner/Presentation/HomeScene/ViewController/DistanceSettingViewController.swift
+++ MateRunner/MateRunner/Presentation/HomeScene/ViewController/DistanceSettingViewController.swift
@@ -11,17 +11,40 @@ import SnapKit
 import UIKit
 
 final class DistanceSettingViewController: UIViewController {
-    var viewModel = DistanceSettingViewModel()
-    var disposeBag = DisposeBag()
+    private let viewModel = DistanceSettingViewModel()
+    private var disposeBag = DisposeBag()
+    
+    private lazy var noticeLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = .darkGray
+        label.font = .notoSans(size: 16, family: .regular)
+        label.text = "거리를 설정해주세요"
+        return label
+    }()
+    
+    private lazy var kilometerLabel: UILabel = {
+        let label = UILabel()
+        label.font = .notoSans(size: 16, family: .regular)
+        label.text = "킬로미터"
+        return label
+    }()
 
     private lazy var distanceTextField: UITextField = {
         let textField = UITextField()
         textField.borderStyle = .none
-        textField.font = UIFont.notoSans(size: 80, family: .black)
+        // textField.font = .notoSans(size: 80, family: .black).italic
+        textField.font = .systemFont(ofSize: 100, weight: .black).italic
         textField.text = "5.00"
+        let attributes = [NSAttributedString.Key.underlineStyle: NSUnderlineStyle.single.rawValue]
+        let attributedString = NSAttributedString(string: "5.00", attributes: attributes)
+        textField.attributedText = attributedString
         textField.keyboardType = .decimalPad
         return textField
     }()
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -29,11 +52,13 @@ final class DistanceSettingViewController: UIViewController {
         self.bindUI()
     }
 
-    override func viewWillDisappear(_ animated: Bool) {
-        super.viewWillDisappear(animated)
-        self.disposeBag = DisposeBag()
-    }
+//    override func viewWillDisappear(_ animated: Bool) {
+//        super.viewWillDisappear(animated)
+//        self.disposeBag = DisposeBag()
+//    }
 }
+
+// MARK: - Private Functions
 
 private extension DistanceSettingViewController {
     func bindUI() {
@@ -42,9 +67,21 @@ private extension DistanceSettingViewController {
     
     func configureUI() {
         self.view.backgroundColor = .systemBackground
+        self.navigationItem.title = "목표 거리"
+        self.view.addSubview(self.noticeLabel)
+        self.noticeLabel.snp.makeConstraints { make in
+            make.centerX.equalToSuperview()
+            make.top.equalTo(self.view.safeAreaLayoutGuide).offset(60)
+        }
         self.view.addSubview(self.distanceTextField)
         self.distanceTextField.snp.makeConstraints { make in
-            make.centerX.centerY.equalToSuperview()
+            make.centerX.equalToSuperview()
+            make.top.equalTo(self.noticeLabel.snp.bottom).offset(20)
+        }
+        self.view.addSubview(self.kilometerLabel)
+        self.kilometerLabel.snp.makeConstraints { make in
+            make.centerX.equalToSuperview()
+            make.top.equalTo(self.distanceTextField.snp.bottom).offset(20)
         }
     }
 }
