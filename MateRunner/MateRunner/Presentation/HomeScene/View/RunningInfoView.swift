a2977adb15d25dfbb626a8eb9d7a299010bda57b
jungwonlee <dev.jungwon.lee@gmail.com>
2021-11-04 16:48:01 +0900

    [Refactor] 리뷰사항 반영

--- MateRunner/MateRunner/Presentation/HomeScene/View/RunningInfoView.swift
+++ MateRunner/MateRunner/Presentation/HomeScene/View/RunningInfoView.swift
@@ -8,8 +8,6 @@
 import UIKit
 
 final class RunningInfoView: UIStackView {
-    private let valueLabel = UILabel()
-    
     convenience init(name: String, value: String, isLarge: Bool = false) {
         self.init(frame: .zero)
         configureUI(name: name, value: value, isLarge: isLarge)
@@ -18,30 +16,31 @@ final class RunningInfoView: UIStackView {
 
 // MARK: - Private Functions
 
-extension RunningInfoView {
+private extension RunningInfoView {
     func configureUI(name: String, value: String, isLarge: Bool) {
         let nameLabel = UILabel()
-        
-        if isLarge {
-            self.valueLabel.font = .notoSansBoldItalic(size: 100)
-            self.spacing = -15
-        } else {
-            self.valueLabel.font = .notoSans(size: 30, family: .bold)
-        }
+        let valueLabel = UILabel()
         
         nameLabel.font = .notoSans(size: 16, family: .regular)
         nameLabel.textColor = .darkGray
-        self.valueLabel.text = value
         nameLabel.text = name
+        
+        if isLarge {
+            valueLabel.font = .notoSansBoldItalic(size: 100)
+            self.spacing = -15
+        } else {
+            valueLabel.font = .notoSans(size: 30, family: .bold)
+        }
+        valueLabel.text = value
         
         self.axis = .vertical
         self.alignment = .center
-        
         self.addArrangedSubview(valueLabel)
         self.addArrangedSubview(nameLabel)
     }
     
     func updateValue(newValue: String) {
-        self.valueLabel.text = newValue
+        guard let valueLabel = self.subviews.first as? UILabel else { return }
+        valueLabel.text = newValue
     }
 }
