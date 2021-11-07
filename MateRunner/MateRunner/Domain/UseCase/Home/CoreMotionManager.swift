73ff1b29c129a27a70da22b8268e6a41814db5e8
lee-yujinn <leeyj7988@naver.com>
2021-11-07 17:40:50 +0900

    [Feat] Mets enum 추가 및 CoreMotionManager Activity update 추가

--- MateRunner/MateRunner/Domain/UseCase/Home/CoreMotionManager.swift
+++ MateRunner/MateRunner/Domain/UseCase/Home/CoreMotionManager.swift
@@ -12,10 +12,11 @@ import RxSwift
 
 final class CoreMotionManager {
     private let pedometer = CMPedometer()
+    private let activityManager = CMMotionActivityManager()
     
     func startPedometer() -> Observable<Double> {
-        return Observable.create { observe in
-            self.pedometer.startUpdates(from: Date()) { pedometerData, error in
+        return Observable.create { [weak self] observe in
+            self?.pedometer.startUpdates(from: Date()) { pedometerData, error in
                 guard let pedometerData = pedometerData, error == nil else { return }
                 if let distance = pedometerData.distance {
                     observe.onNext(distance.doubleValue)
@@ -26,9 +27,21 @@ final class CoreMotionManager {
         }
     }
     
-//    func startActivity() -> Observable<Double> {
-//        
-//    }
+    func startActivity() -> Observable<Double> {
+        return Observable.create { [weak self] observe in
+            self?.activityManager.startActivityUpdates(to: .current ?? .main) { activity in
+                guard let activity = activity else { return }
+                if activity.walking {
+                    observe.onNext(Mets.walking.rawValue)
+                }
+                if activity.running {
+                    observe.onNext(Mets.running.rawValue)
+                }
+            }
+            observe.onCompleted()
+            return Disposables.create()
+        }
+    }
     
     func stopPedometer() {
         self.pedometer.stopUpdates()
---
+++ MateRunner/MateRunner/Util/Constant/Mets.swift
@@ -0,0 +1,13 @@
+//
+//  Mets.swift
+//  MateRunner
+//
+//  Created by 이유진 on 2021/11/07.
+//
+
+import Foundation
+
+enum Mets: Double {
+    case walking = 3.8
+    case running = 10.0
+}
