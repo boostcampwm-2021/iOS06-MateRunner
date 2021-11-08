e8c83c9f7f9d2b60766c2cf46a972e3984906911
YuJin Lee <41044154+lee-yujinn@users.noreply.github.com>
2021-11-08 16:31:18 +0900

    Revert "RxTimeInterval로 타이머 재구성"

--- MateRunner/MateRunner/Domain/UseCase/Home/DefaultRunningPreparationUseCase.swift
+++ MateRunner/MateRunner/Domain/UseCase/Home/DefaultRunningPreparationUseCase.swift
@@ -9,32 +9,22 @@ import Foundation
 
 import RxSwift
 
-final class DefaultRunningPreparationUseCase: RunningPreparationUseCase {
+class DefaultRunningPreparationUseCase: RunningPreparationUseCase {
 	var timeLeft = BehaviorSubject(value: 3)
 	var isTimeOver = BehaviorSubject(value: false)
-	var timerDisposeBag = DisposeBag()
 	private let maxTime = 3
 	
 	func executeTimer() {
-		Observable<Int>
-			.interval(
-				RxTimeInterval.seconds(1),
-				scheduler: MainScheduler.instance
-			)
-			.map { $0 + 1 }
-			.subscribe(onNext: { [weak self] newTime in
-				self?.updateTime(with: newTime)
-			})
-			.disposed(by: self.timerDisposeBag)
-	}
-	private func updateTime(with time: Int) {
-		if time == self.maxTime {
-			self.timerDisposeBag = DisposeBag()
-			self.isTimeOver.onNext(true)
+		var time = 0
+		Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { [weak self] timer in
+			time += 1
+			if time == self?.maxTime {
+				timer.invalidate()
+				self?.isTimeOver.onNext(true)
+			}
+			self?.timeLeft.onNext((self?.maxTime ?? 3) - time)
 		}
-		self.timeLeft.onNext((self.maxTime) - time)
 	}
-	
 	private func checkTimeOver(timeLeft: Int) -> Bool {
 		return timeLeft >= self.maxTime
 	}
