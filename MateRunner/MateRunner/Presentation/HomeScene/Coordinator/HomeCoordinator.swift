a9a735822397d88794bba18fb7fbc019ed60e505
jeonyeohun <21500630@handong.edu>
2021-11-09 16:16:21 +0900

    [Fix] HomeCoordinator 정의

--- MateRunner/MateRunner/Presentation/HomeScene/Coordinator/HomeCoordinator.swift
+++ MateRunner/MateRunner/Presentation/HomeScene/Coordinator/HomeCoordinator.swift
@@ -7,24 +7,52 @@
 
 import UIKit
 
-class HomeCoordinator: Coordinator {
-    weak var parentCoordinator: Coordinator?
-    var childCoordinators: [Coordinator] = []
-    var navigationController: UINavigationController
+protocol HomeCoordinator: Coordinator {
+    func showHomeViewController() -> UIViewController
+    func pushRunningModeSettingViewController()
+    func pushMateRunningModeSettingViewController()
+    func pushDistanceSettingViewController()
+    func pushRunningPreparationViewController()
     
-    init() {
-        self.navigationController = UINavigationController()
-    }
+}
+
+class DefaultHomeCoordinator: HomeCoordinator {
+    weak var finishDelegate: CoordinatorFinishDelegate?
+    var navigationController: UINavigationController
+    var childCoordinators: [Coordinator] = []
+    var type: CoordinatorType { .home }
     
     func start() {
+        
     }
     
-    func pushToHome() -> UINavigationController {
+    required init(_ navigationController: UINavigationController) {
+        self.navigationController = navigationController
+    }
+    
+    func showHomeViewController() -> UIViewController {
         let homeViewController = HomeViewController()
-        homeViewController.delegate = self
-        homeViewController.view.backgroundColor = .white
-        navigationController.setViewControllers([homeViewController], animated: true)
-        
-        return navigationController
+        homeViewController.coordinator = self
+        return homeViewController
+    }
+    
+    func pushRunningModeSettingViewController() {
+        let runningModeSettingViewController = RunningModeSettingViewController()
+        self.navigationController.pushViewController(runningModeSettingViewController, animated: true)
+    }
+    
+    func pushMateRunningModeSettingViewController() {
+        let mateRunningModeSettingViewController = MateRunningModeSettingViewController()
+        self.navigationController.pushViewController(mateRunningModeSettingViewController, animated: true)
+    }
+    
+    func pushDistanceSettingViewController() {
+        let distanceSettingViewController = DistanceSettingViewController()
+        self.navigationController.pushViewController(distanceSettingViewController, animated: true)
+    }
+    
+    func pushRunningPreparationViewController() {
+        let runningPreparationViewController = RunningPreparationViewController()
+        self.navigationController.pushViewController(runningPreparationViewController, animated: true)
     }
 }
