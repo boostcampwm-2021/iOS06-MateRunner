d0404b1dc500163bf3b6d91957c92637ac5cfaad
lee-yujinn <leeyj7988@naver.com>
2021-11-09 17:14:53 +0900

    [Feat] headerView 추가

--- MateRunner/MateRunner/Presentation/MateScene/View/MateHeaderView.swift
+++ MateRunner/MateRunner/Presentation/MateScene/View/MateHeaderView.swift
@@ -1,5 +1,5 @@
 //
-//  mateHeaderView.swift
+//  MateHeaderView.swift
 //  MateRunner
 //
 //  Created by 이유진 on 2021/11/09.
@@ -7,14 +7,24 @@
 
 import UIKit
 
-class mateHeaderView: UITableViewHeaderFooterView {
-
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+class MateHeaderView: UITableViewHeaderFooterView {
+    static let identifier = "mateHeaderView"
+    
+    private lazy var headerTitleLable: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.notoSans(size: 18, family: .medium)
+        return label
+    }()
+    
+    private func configureUI() {
+        addSubview(headerTitleLable)
+        self.headerTitleLable.snp.makeConstraints { make in
+            make.left.equalToSuperview().offset(20)
+            make.centerY.equalToSuperview()
+        }
     }
-    */
-
+    
+    func updateUI(value: Int) {
+        self.headerTitleLable.text = "친구 (\(value)명)"
+    }
 }
