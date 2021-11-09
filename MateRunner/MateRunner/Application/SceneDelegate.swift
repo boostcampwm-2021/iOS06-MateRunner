12e43ea2e1126a628a9402d04748a5808755e699
jeonyeohun <21500630@handong.edu>
2021-11-09 13:50:37 +0900

    [Feat] 앱 시작 시 appCoordinator를 사용하도록 변경

--- MateRunner/MateRunner/Application/SceneDelegate.swift
+++ MateRunner/MateRunner/Application/SceneDelegate.swift
@@ -10,14 +10,19 @@ import UIKit
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
+    var appCoordinator: AppCoordinator?
 
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         self.window = UIWindow(windowScene: windowScene)
-        self.window?.rootViewController = TabBarController()
-        self.window?.makeKeyAndVisible()
-        self.window?.tintColor = .mrPurple
+        let navigationController: UINavigationController = .init()
+        
+        window?.rootViewController = navigationController
+        window?.makeKeyAndVisible()
+        
+        appCoordinator = AppCoordinator.init(navigationController)
+        appCoordinator?.start()
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
