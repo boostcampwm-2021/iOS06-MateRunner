0abd5996e4945a7a778037580c7139f837d2ac9a
jeonyeohun <21500630@handong.edu>
2021-11-09 13:01:31 +0900

    [Feat] 탭바 관련 enum 추가

---
+++ MateRunner/MateRunner/Util/Constant/TabBarPage.swift
@@ -0,0 +1,35 @@
+//
+//  TabBarPage.swift
+//  MateRunner
+//
+//  Created by 전여훈 on 2021/11/09.
+//
+
+import Foundation
+
+enum TabBarPage: String {
+    case home, record, mate, mypage
+    
+    init?(index: Int) {
+        switch index {
+        case 0: self = .home
+        case 1: self = .record
+        case 2: self = .mate
+        case 3: self = .mypage
+        default: return nil
+        }
+    }
+    
+    func pageOrderNumber() -> Int {
+        switch self {
+        case .home: return 0
+        case .record: return 1
+        case .mate: return 2
+        case .mypage: return 3
+        }
+    }
+    
+    func tabIconName() -> String {
+        return self.rawValue
+    }
+}
