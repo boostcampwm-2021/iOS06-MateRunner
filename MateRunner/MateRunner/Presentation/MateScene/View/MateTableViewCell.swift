a7fb5b4a7ba628062ce934467628a90627452634
lee-yujinn <leeyj7988@naver.com>
2021-11-09 17:01:28 +0900

    [Feat] TableView Cell 추가

--- MateRunner/MateRunner/Presentation/MateScene/View/MateTableViewCell.swift
+++ MateRunner/MateRunner/Presentation/MateScene/View/MateTableViewCell.swift
@@ -8,7 +8,21 @@
 import UIKit
 
 class MateTableViewCell: UITableViewCell {
-
+    static let identifier = "mateTableViewCell"
+    
+    private lazy var mateProfileImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.layer.masksToBounds = true
+        imageView.layer.cornerRadius = 50
+        return imageView
+    }()
+    
+    private lazy var mateNameLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.notoSans(size: 16, family: .medium)
+        return label
+    }()
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
