ab9ef5312ce6771d0f831ff66f1bc1a486a460fc
jeonyeohun <21500630@handong.edu>
2021-11-09 11:43:06 +0900

    [Feat] 코디네이터 적용을 위한 Coordinating 프로토콜 추가

---
+++ MateRunner/MateRunner/Presentation/Common/Coordinator/Coordinating.swift
@@ -0,0 +1,13 @@
+//
+//  Coordinating.swift
+//  MateRunner
+//
+//  Created by 전여훈 on 2021/11/09.
+//
+
+import Foundation
+
+protocol Coordinating {
+	var childCoordinators: [Coordinating] { get set }
+	func start()
+}
