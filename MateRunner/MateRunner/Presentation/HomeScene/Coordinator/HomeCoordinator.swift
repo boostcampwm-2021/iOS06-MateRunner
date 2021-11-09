8a98628774f168181f786244fdb14b83c462011c
jeonyeohun <21500630@handong.edu>
2021-11-09 11:51:51 +0900

    [Feat] 홈 탭에 대한 HomeCoordinator 작성

---
+++ MateRunner/MateRunner/Presentation/HomeScene/Coordinator/HomeCoordinator.swift
@@ -0,0 +1,31 @@
+//
+//  HomeCoordinator.swift
+//  MateRunner
+//
+//  Created by 전여훈 on 2021/11/09.
+//
+
+import UIKit
+
+class HomeCoordinator: Coordinating {
+	weak var parentCoordinator: Coordinating?
+	var childCoordinators: [Coordinating] = []
+	var navigationController: UINavigationController
+	
+	init() {
+		self.navigationController = UINavigationController()
+	}
+	
+	func start() {
+		
+	}
+	
+	func pushToHome() -> UINavigationController {
+		let homeViewController = HomeViewController()
+		homeViewController.delegate = self
+		homeViewController.view.backgroundColor = .white
+		navigationController.setViewControllers([homeViewController], animated: true)
+		
+		return navigationController
+	}
+}
