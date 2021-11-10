09be8cac4f1c0c5c16c642ad867e55e835597260
jeonyeohun <21500630@handong.edu>
2021-11-10 21:13:23 +0900

    [Refactor] 인덴테이션 수정

--- MateRunner/MateRunner/Presentation/HomeScene/ViewModel/DistanceSettingViewModel.swift
+++ MateRunner/MateRunner/Presentation/HomeScene/ViewModel/DistanceSettingViewModel.swift
@@ -11,45 +11,45 @@ import RxCocoa
 import RxSwift
 
 final class DistanceSettingViewModel {
-	private let distanceSettingUseCase: DistanceSettingUseCase
+    private let distanceSettingUseCase: DistanceSettingUseCase
     private let runningSettngUseCase: RunningSettingUseCase
     private weak var coordinator: SettingCoordinator?
-	
-	struct Input {
-		let distance: Observable<String>
-		let doneButtonDidTapEvent: Observable<Void>
+    
+    struct Input {
+        let distance: Observable<String>
+        let doneButtonDidTapEvent: Observable<Void>
         let startButtonDidTapEvent: Observable<Void>
-	}
-	
-	struct Output {
-		@BehaviorRelayProperty var distanceFieldText: String? = "5.00"
+    }
+    
+    struct Output {
+        @BehaviorRelayProperty var distanceFieldText: String? = "5.00"
         let keyboardShouldhide = PublishRelay<Bool>()
-	}
-	
+    }
+    
     init(
         coordinator: SettingCoordinator,
         distanceSettingUseCase: DistanceSettingUseCase,
         runningSettngUseCase: RunningSettingUseCase
     ) {
         self.coordinator = coordinator
-		self.distanceSettingUseCase = distanceSettingUseCase
+        self.distanceSettingUseCase = distanceSettingUseCase
         self.runningSettngUseCase = runningSettngUseCase
-	}
-	
-	func transform(from input: Input, disposeBag: DisposeBag) -> Output {
-		let output = Output()
-		input.distance
-			.subscribe(onNext: { [weak self] distance in
+    }
+    
+    func transform(from input: Input, disposeBag: DisposeBag) -> Output {
+        let output = Output()
+        input.distance
+            .subscribe(onNext: { [weak self] distance in
                 self?.distanceSettingUseCase.validate(text: distance)
             })
-			.disposed(by: disposeBag)
-		
-		input.doneButtonDidTapEvent
-			.withLatestFrom(input.distance)
-			.map(self.padZeros)
-			.map(self.convertInvalidDistance)
+            .disposed(by: disposeBag)
+        
+        input.doneButtonDidTapEvent
+            .withLatestFrom(input.distance)
+            .map(self.padZeros)
+            .map(self.convertInvalidDistance)
             .bind(to: output.$distanceFieldText)
-			.disposed(by: disposeBag)
+            .disposed(by: disposeBag)
         
         input.doneButtonDidTapEvent
             .map {true}
@@ -63,14 +63,14 @@ final class DistanceSettingViewModel {
                 )
             })
             .disposed(by: disposeBag)
-		
-		self.distanceSettingUseCase.validatedText
-			.scan("") { oldValue, newValue in
-				newValue == nil ? oldValue : newValue
-			}
-			.map({ self.configureZeros(from: $0 ?? "0") })
-			.bind(to: output.$distanceFieldText)
-			.disposed(by: disposeBag)
+        
+        self.distanceSettingUseCase.validatedText
+            .scan("") { oldValue, newValue in
+                newValue == nil ? oldValue : newValue
+            }
+            .map({ self.configureZeros(from: $0 ?? "0") })
+            .bind(to: output.$distanceFieldText)
+            .disposed(by: disposeBag)
         
         self.distanceSettingUseCase.validatedText
             .distinctUntilChanged()
@@ -79,36 +79,36 @@ final class DistanceSettingViewModel {
                 self.runningSettngUseCase.updateTargetDistance(distance: newDistance)
             })
             .disposed(by: disposeBag)
-		
-		return output
-	}
+        
+        return output
+    }
     private func convertToDouble(from distance: String?) -> Double? {
         guard let distance = distance else { return nil }
         return Double(distance)
     }
-	
-	private func convertInvalidDistance(from text: String) -> String {
-		guard text != "0.00" else { return "5.00" }
-		return text
-	}
-	
-	private func configureZeros(from text: String) -> String {
-		guard !text.isEmpty else { return "0" }
-		guard !text.contains(".") && text.first == "0" else { return text }
-		return "\(text.last ?? "0")"
-	}
-	
-	private func padZeros(text: String) -> String {
-		var paddedText = text
-		if paddedText.isEmpty { paddedText = "0" }
-		return paddedText.contains(".") ? self.addZeros(to: paddedText) : paddedText + ".00"
-	}
-	
-	private func addZeros(to text: String) -> String {
-		var paddedText = text
-		while paddedText.components(separatedBy: ".")[1].count < 2 {
-			paddedText.append("0")
-		}
-		return paddedText
-	}
+    
+    private func convertInvalidDistance(from text: String) -> String {
+        guard text != "0.00" else { return "5.00" }
+        return text
+    }
+    
+    private func configureZeros(from text: String) -> String {
+        guard !text.isEmpty else { return "0" }
+        guard !text.contains(".") && text.first == "0" else { return text }
+        return "\(text.last ?? "0")"
+    }
+    
+    private func padZeros(text: String) -> String {
+        var paddedText = text
+        if paddedText.isEmpty { paddedText = "0" }
+        return paddedText.contains(".") ? self.addZeros(to: paddedText) : paddedText + ".00"
+    }
+    
+    private func addZeros(to text: String) -> String {
+        var paddedText = text
+        while paddedText.components(separatedBy: ".")[1].count < 2 {
+            paddedText.append("0")
+        }
+        return paddedText
+    }
 }
