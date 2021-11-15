59207cc3617d7aebd952444c6d8bb6b1a029c507
jeonyeohun <21500630@handong.edu>
2021-11-16 00:30:55 +0900

    [Feat] RxTimerService 생성 및 정의

---
+++ MateRunner/MateRunner/Util/Service/RxTimerService.swift
@@ -0,0 +1,27 @@
+//
+//  RxTimerService.swift
+//  MateRunner
+//
+//  Created by 전여훈 on 2021/11/16.
+//
+
+import Foundation
+
+import RxSwift
+
+class RxTimerService {
+    private(set) var disposeBag = DisposeBag()
+    
+    func start() -> Observable<Int> {
+        return Observable<Int>
+            .interval(
+                RxTimeInterval.seconds(1),
+                scheduler: MainScheduler.instance
+            )
+            .map { $0 + 1 }
+    }
+    
+    func stop() {
+        self.disposeBag = DisposeBag()
+    }
+}
