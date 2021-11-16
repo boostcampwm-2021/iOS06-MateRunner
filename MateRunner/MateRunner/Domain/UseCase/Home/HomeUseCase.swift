41a98c94df829efaaa3b056369a919a7259d8067
jeonyeohun <21500630@handong.edu>
2021-11-17 01:19:31 +0900

    [Feat] 권한관련 데이터 바인딩, 알러트 표시

--- MateRunner/MateRunner/Domain/UseCase/Home/HomeUseCase.swift
+++ MateRunner/MateRunner/Domain/UseCase/Home/HomeUseCase.swift
@@ -5,8 +5,45 @@
 //  Created by 전여훈 on 2021/11/09.
 //
 
+import CoreLocation
 import Foundation
 
-final class HomeUseCase {
+import RxSwift
 
+enum LocationAuthorizationStatus {
+    case allowed, disallowed, notDetermined
+}
+
+final class HomeUseCase {
+    private let locationService: LocationService
+    var authorizationStatus = BehaviorSubject<LocationAuthorizationStatus?>(value: nil)
+    var disposeBag = DisposeBag()
+    
+    init(locationService: LocationService) {
+        self.locationService = locationService
+    }
+    
+    func checkAuthorization() {
+        self.locationService.requestAuthorization()
+            .subscribe(onNext: { [weak self] status in
+                switch status {
+                case .authorizedAlways, .authorizedWhenInUse:
+                    self?.authorizationStatus.onNext(.allowed)
+                case .notDetermined:
+                    self?.authorizationStatus.onNext(.notDetermined)
+                case .denied, .restricted:
+                    self?.authorizationStatus.onNext(.disallowed)
+                @unknown default:
+                    self?.authorizationStatus.onNext(nil)
+                }
+            })
+            .disposed(by: self.disposeBag)
+    }
+    
+    func fetchUserLocation() -> Observable<CLLocation> {
+        self.locationService.start()
+        return self.locationService.observeUpdatedLocation()
+            .compactMap({ $0.last })
+            .asObservable()
+    }
 }
--- MateRunner/MateRunner/Presentation/HomeScene/ViewModel/HomeViewModel.swift
+++ MateRunner/MateRunner/Presentation/HomeScene/ViewModel/HomeViewModel.swift
@@ -5,7 +5,11 @@
 //  Created by 전여훈 on 2021/11/09.
 //
 
+import CoreLocation
 import Foundation
+
+import RxRelay
+import RxSwift
 
 final class HomeViewModel {
     weak var coordinator: HomeCoordinator?
@@ -14,6 +18,32 @@ final class HomeViewModel {
     init(coordinator: HomeCoordinator, homeUseCase: HomeUseCase) {
         self.coordinator = coordinator
         self.homeUseCase = homeUseCase
+    }
+    
+    struct Input {
+        let viewDidLoadEvent: Observable<Void>
+        let startButtonDidTapEvent: Observable<Void>
+    }
+    struct Output {
+        let currentUserLocation = PublishRelay<CLLocationCoordinate2D>()
+        let authorizationAlertShouldShow = BehaviorRelay<Bool>(value: false)
+    }
+    
+    func transform(input: Input, disposeBag: DisposeBag) -> Output {
+        let output = Output()
+        
+        input.viewDidLoadEvent
+            .subscribe({ _ in
+                self.homeUseCase.checkAuthorization()
+            })
+            .disposed(by: disposeBag)
+        
+        self.homeUseCase.authorizationStatus
+            .map({ $0 == .disallowed })
+            .bind(to: output.authorizationAlertShouldShow)
+            .disposed(by: disposeBag)
+        
+        return output
     }
     
     func startButtonDidTap() {
