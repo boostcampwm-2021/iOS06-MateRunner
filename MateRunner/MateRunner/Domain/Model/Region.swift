f299dcda10e4656c3d3da19d4bbb0a38d4122392
jeonyeohun <21500630@handong.edu>
2021-11-17 11:38:23 +0900

    [Feat] Region, Point extension 추가

---
+++ MateRunner/MateRunner/Domain/Model/Region.swift
@@ -0,0 +1,24 @@
+//
+//  Region.swift
+//  MateRunner
+//
+//  Created by 전여훈 on 2021/11/17.
+//
+
+import CoreLocation
+import Foundation
+
+struct Region {
+    private let center: CLLocationCoordinate2D
+    private let span: (Double, Double)
+    
+    init() {
+        self.center = CLLocationCoordinate2DMake(0, 0)
+        self.span = (0, 0)
+    }
+    
+    init(center: CLLocationCoordinate2D, span: (Double, Double)) {
+        self.center = center
+        self.span = span
+    }
+}
