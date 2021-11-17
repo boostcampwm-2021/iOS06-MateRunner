f22d22c4d257c44ae6042937292c09a10177aeb6
lee-yujinn <leeyj7988@naver.com>
2021-11-17 16:36:47 +0900

    [Feat] 친구 검색 뷰 서치바,테이블 뷰

--- MateRunner/MateRunner/Presentation/MateScene/ViewController/AddMateViewController.swift
+++ MateRunner/MateRunner/Presentation/MateScene/ViewController/AddMateViewController.swift
@@ -7,6 +7,64 @@
 
 import UIKit
 
+import RxSwift
+
 final class AddMateViewController: UIViewController {
+//    var mateViewModel: MateViewModel?
+    private let disposeBag = DisposeBag()
     
+    private lazy var mateSearchBar: UISearchBar = {
+        let searchBar = UISearchBar()
+        searchBar.placeholder = "닉네임을 입력해주세요."
+        searchBar.backgroundImage = UIImage()
+        return searchBar
+    }()
+    
+    private lazy var mateTableView: UITableView = {
+        let tableView = UITableView()
+        tableView.separatorStyle = .none
+//        tableView.delegate = self
+//        tableView.dataSource = self
+//        tableView.register(MateTableViewCell.self, forCellReuseIdentifier: MateTableViewCell.identifier)
+//        tableView.register(MateHeaderView.self, forHeaderFooterViewReuseIdentifier: MateHeaderView.identifier)
+        return tableView
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.configureUI()
+        self.addEmptyView()
+    }
+}
+
+// MARK: - Private Functions
+
+private extension AddMateViewController {
+    func configureUI() {
+        self.navigationItem.title = "친구 검색"
+        self.view.backgroundColor = .white
+        
+        self.view.addSubview(self.mateSearchBar)
+        self.mateSearchBar.snp.makeConstraints { make in
+            make.top.left.right.equalTo(self.view.safeAreaLayoutGuide).inset(0)
+        }
+        
+        self.view.addSubview(self.mateTableView)
+        self.mateTableView.snp.makeConstraints { make in
+            make.top.equalTo(self.mateSearchBar.snp.bottom).offset(0)
+            make.right.left.bottom.equalToSuperview()
+        }
+    }
+    
+    func addEmptyView() {
+        let emptyView = MateEmptyView(
+            title: "닉네임을 검색해서 메이트를 추가해보세요!",
+            topOffset: 180
+        )
+        
+        self.mateTableView.addSubview(emptyView)
+        emptyView.snp.makeConstraints { make in
+            make.centerX.equalToSuperview()
+        }
+    }
 }
