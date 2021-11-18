6e42bce0999e8a8dbd7be207a2f8374233a187fb
lee-yujinn <leeyj7988@naver.com>
2021-11-18 16:54:16 +0900

    [Feat] 메이트 요청 data 구조체

--- MateRunner/MateRunner/Domain/Model/MateRequest.swift
+++ MateRunner/MateRunner/Domain/Model/MateRequest.swift
@@ -6,3 +6,18 @@
 //
 
 import Foundation
+
+struct MateRequest: Codable {
+    let sender: String
+
+    init(sender: String) {
+        self.sender = sender
+    }
+    
+    init?(from dictionary: [AnyHashable: Any]) {
+        guard let sender = dictionary["sender"] as? String else {
+                  return nil
+              }
+        self.init(sender: sender)
+    }
+}
