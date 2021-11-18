21d094142426b0422c6d9b48fcfa88360a9ac4e8
lee-yujinn <leeyj7988@naver.com>
2021-11-19 00:41:29 +0900

    [Feat] 친구 프로필 ViewController 추가

--- MateRunner/MateRunner/Presentation/MateScene/View/MateProfileHeaderView.swift
+++ MateRunner/MateRunner/Presentation/MateScene/View/MateProfileHeaderView.swift
@@ -7,6 +7,8 @@
 
 import UIKit
 
+import SnapKit
+
 final class MateProfileHeaderView: UITableViewHeaderFooterView {
     static var identifier: String {
         return String(describing: Self.self)
@@ -26,7 +28,19 @@ final class MateProfileHeaderView: UITableViewHeaderFooterView {
         return imageView
     }()
     
+    private lazy var mateNickname: UILabel = {
+        let label = UILabel()
+        label.font = .notoSans(size: 18, family: .bold)
+        return label
+    }()
+    
     private lazy var cumulativeRecordView = CumulativeRecordView()
+    
+    private lazy var dateLabel: UILabel = {
+        let label = UILabel()
+        label.font = .notoSans(size: 20, family: .medium)
+        return label
+    }()
     
     override init(reuseIdentifier: String?) {
         super.init(reuseIdentifier: Self.identifier)
@@ -38,7 +52,10 @@ final class MateProfileHeaderView: UITableViewHeaderFooterView {
         self.configureUI()
     }
     
-    func updateUI() {
+    func updateUI(time: String, distance: String, calorie: String) {
+        self.cumulativeRecordView.timeLabel.text = time
+        self.cumulativeRecordView.distanceLabel.text = distance
+        self.cumulativeRecordView.calorieLabel.text = calorie
     }
 }
 
@@ -46,7 +63,35 @@ final class MateProfileHeaderView: UITableViewHeaderFooterView {
 
 private extension MateProfileHeaderView {
     func configureUI() {
+        self.addSubview(self.shadowView)
+        self.shadowView.snp.makeConstraints { make in
+            make.left.right.top.equalToSuperview()
+            make.height.equalTo(344)
+        }
         
+        self.shadowView.addSubview(self.mateProfileImageView)
+        self.mateProfileImageView.snp.makeConstraints { make in
+            make.centerX.equalToSuperview()
+            make.bottom.equalToSuperview().offset(156)
+        }
+        
+        self.shadowView.addSubview(self.mateNickname)
+        self.mateNickname.snp.makeConstraints { make in
+            make.centerX.equalToSuperview()
+            make.top.equalTo(self.mateProfileImageView).offset(10)
+        }
+        
+        self.shadowView.addSubview(self.cumulativeRecordView)
+        self.cumulativeRecordView.snp.makeConstraints { make in
+            make.centerX.equalToSuperview()
+            make.top.equalTo(self.mateNickname).offset(22)
+        }
+        
+        self.dateLabel.snp.makeConstraints { make in
+            make.top.equalTo(self.shadowView.snp.bottom).offset(44)
+            make.width.equalTo(305)
+            make.centerX.equalToSuperview()
+        }
     }
 }
     
--- MateRunner/MateRunner/Presentation/MateScene/ViewController/MateProfileViewController.swift
+++ MateRunner/MateRunner/Presentation/MateScene/ViewController/MateProfileViewController.swift
@@ -8,10 +8,76 @@
 import UIKit
 
 class MateProfileViewController: UIViewController {
-
+    private lazy var tableView: UITableView = {
+        let tableView = UITableView()
+        tableView.separatorStyle = .none
+        tableView.showsVerticalScrollIndicator = false
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.register(
+            MateTableViewCell.self,
+            forCellReuseIdentifier: MateTableViewCell.identifier
+        )
+        tableView.register(
+            MateProfileHeaderView.self,
+            forHeaderFooterViewReuseIdentifier: MateProfileHeaderView.identifier
+        )
+        return tableView
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
     }
 
+}
+
+// MARK: - Private Functions
+
+private extension MateProfileViewController {
+
+}
+
+// MARK: - UITableViewDelegate
+
+extension MateProfileViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 130
+    }
+}
+
+// MARK: - UITableViewDataSource
+
+extension MateProfileViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 400
+    }
+    
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        guard let header = tableView.dequeueReusableHeaderFooterView(
+            withIdentifier: MateProfileHeaderView.identifier) as? MateProfileHeaderView else {
+                return UITableViewHeaderFooterView()
+            }
+        header.updateUI(time: "00:43", distance: "0.02", calorie: "143")
+        
+        return header
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(
+            withIdentifier: MateTableViewCell.identifier,
+            for: indexPath) as? MateTableViewCell else { return UITableViewCell() }
+//
+//        let mate = self.mateViewModel?.filteredMate[indexPath.row]
+//        cell.updateUI(name: mate?.key ?? "", image: mate?.value ?? "")
+        
+        return cell
+    }
+    
+//    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+//    }
 }
