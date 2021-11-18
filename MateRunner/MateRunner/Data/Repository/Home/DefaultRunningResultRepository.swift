63f8bee42ebbf854eaae2b5275f0bcfb300a69dd
jeonyeohun <21500630@handong.edu>
2021-11-18 11:31:38 +0900

    [Feat] 에러타입 추가, userdefault persistence로 사용자 닉네임 가져오도록 구현

--- MateRunner/MateRunner/Data/Repository/Home/DefaultRunningResultRepository.swift
+++ MateRunner/MateRunner/Data/Repository/Home/DefaultRunningResultRepository.swift
@@ -11,17 +11,27 @@ import RxSwift
 
 final class DefaultRunningResultRepository: RunningResultRepository {
     let networkService = DefaultFireStoreNetworkService()
+    let userDefaultPersistence = DefaultUserDefaultPersistence()
+    
+    private func fetchUserNickname() -> String? {
+        return self.userDefaultPersistence.getStringValue(key: .nickname)
+    }
     
     func saveRunningResult(_ runningResult: RunningResult?) -> Observable<Void> {
-        guard let runningResult = runningResult else {
+        guard let runningResult = runningResult,
+              let startDateTime = runningResult.dateTime else {
             return Observable.error(FirebaseServiceError.nilDataError)
+        }
+
+        guard let userNickName = self.fetchUserNickname() else {
+            return Observable.error(FirebaseServiceError.userNicknameNotExistsError)
         }
         
         return self.networkService.writeDTO(
             RunningResultDTO(from: runningResult),
             collection: FirebaseCollection.runningResult,
-            document: "hunihun956",
-            key: runningResult.dateTime?.fullDateTimeString() ?? Date().fullDateTimeString()
+            document: userNickName,
+            key: startDateTime.fullDateTimeString()
         )
     }
 }
