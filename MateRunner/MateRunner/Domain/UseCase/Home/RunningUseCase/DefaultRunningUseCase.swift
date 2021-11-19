49301bf93246e365b6349c6f542858304157743d
jeonyeohun <21500630@handong.edu>
2021-11-19 23:41:58 +0900

    [Feat] 모드별 종료조건 추가

--- MateRunner/MateRunner/Domain/UseCase/Home/RunningUseCase/DefaultRunningUseCase.swift
+++ MateRunner/MateRunner/Domain/UseCase/Home/RunningUseCase/DefaultRunningUseCase.swift
@@ -12,7 +12,15 @@ import RxSwift
 
 final class DefaultRunningUseCase: RunningUseCase {
     private var points: [Point]
-    private var currentMETs = 0.0
+    private var currentMETs: Double
+    private var disposeBag: DisposeBag
+    
+    private let cancelTimer: RxTimerService
+    private let popUpTimer: RxTimerService
+    private let runningTimer: RxTimerService
+    private let coreMotionService: CoreMotionService
+    private let runningRepository: RunningRepository
+    private let userRepository: UserRepository
     
     var runningSetting: RunningSetting
     var runningData: BehaviorSubject<RunningData>
@@ -25,16 +33,6 @@ final class DefaultRunningUseCase: RunningUseCase {
     var cancelTimeLeft: PublishSubject<Int>
     var popUpTimeLeft: PublishSubject<Int>
     
-    private let cancelTimer: RxTimerService
-    private let popUpTimer: RxTimerService
-    private let runningTimer: RxTimerService
-    private let coreMotionService: CoreMotionService
-    
-    private let runningRepository: RunningRepository
-    private let userRepository: UserRepository
-    
-    private var disposeBag = DisposeBag()
-    
     init(
         runningSetting: RunningSetting,
         cancelTimer: RxTimerService,
@@ -44,6 +42,10 @@ final class DefaultRunningUseCase: RunningUseCase {
         runningRepository: RunningRepository,
         userRepository: UserRepository
     ) {
+        self.points = []
+        self.currentMETs = 0.0
+        self.disposeBag = DisposeBag()
+        
         self.runningSetting = runningSetting
         self.cancelTimer = cancelTimer
         self.runningTimer = runningTimer
@@ -51,7 +53,7 @@ final class DefaultRunningUseCase: RunningUseCase {
         self.coreMotionService = coreMotionService
         self.runningRepository = runningRepository
         self.userRepository = userRepository
-        self.points = []
+        
         self.runningData = BehaviorSubject(value: RunningData())
         self.isCanceled  = BehaviorSubject(value: false)
         self.isFinished = BehaviorSubject(value: false)
@@ -126,6 +128,7 @@ final class DefaultRunningUseCase: RunningUseCase {
         self.shouldShowPopUp.onNext(false)
         self.cancelTimeLeft.onNext(3)
         self.cancelTimer.stop()
+        self.stopListeningMate()
     }
     
     func listenMateRunningRealTimeData() {
@@ -133,19 +136,28 @@ final class DefaultRunningUseCase: RunningUseCase {
               let hostNickname = self.runningSetting.hostNickname,
               let mateNickname = self.runningSetting.mateNickname,
               let userNickname = self.userNickname() else { return }
-        
+
         let mate = userNickname == hostNickname ? mateNickname : hostNickname
+
         self.runningRepository.listen(sessionId: sessionId, mate: mate)
             .subscribe(onNext: { [weak self] mateRunningRealTimeData in
                 guard let self = self,
                       let currentData = try? self.runningData.value() else { return }
-                self.runningData.onNext(currentData.makeCopy(mateRunningRealTimeData: mateRunningRealTimeData))
-                self.updateProgress(self.mateProgress, value: mateRunningRealTimeData.elapsedDistance)
+                
+                self.runningData.onNext(
+                    currentData.makeCopy(mateRunningRealTimeData: mateRunningRealTimeData)
+                )
+                self.updateProgress(
+                    self.mateProgress,
+                    value: mateRunningRealTimeData.elapsedDistance
+                )
                 self.updateProgress(
                     self.totalProgress,
                     value: currentData.makeCopy(
-                        mateRunningRealTimeData: mateRunningRealTimeData).totalElapsedDistance
+                        mateRunningRealTimeData: mateRunningRealTimeData
+                    ).totalElapsedDistance
                 )
+                self.checkRunningShouldFinish(value: currentData.myElapsedDistance)
             })
             .disposed(by: self.disposeBag)
     }
@@ -165,6 +177,12 @@ final class DefaultRunningUseCase: RunningUseCase {
         )
     }
     
+    private func stopListeningMate() {
+        guard let sessionId = self.runningSetting.sessionId,
+              let mateNickname = self.runningSetting.mateNickname else { return }
+        self.runningRepository.stopListen(sessionId: sessionId, mate: mateNickname)
+    }
+    
     private func userNickname() -> String? {
         return self.userRepository.fetchUserNickname()
     }
@@ -175,10 +193,39 @@ final class DefaultRunningUseCase: RunningUseCase {
     
     private func checkRunningShouldFinish(value: Double) {
         guard let targetDistance = self.runningSetting.targetDistance,
-              value >= self.convertToMeter(value: targetDistance) else { return }
+              let mode = self.runningSetting.mode,
+              let runningData = try? self.runningData.value() else { return }
+        
+        guard mode == .team
+        ? checkTotalDistanceSatisfy(
+            targetDistace: targetDistance,
+            myDistance: runningData.myElapsedDistance,
+            mateDistance: runningData.mateElapsedDistance
+        )
+        : checkMyDistanceSatisfy(
+            targetDistace: targetDistance,
+            myDistance: runningData.myElapsedDistance
+        ) else { return }
+        
         self.isFinished.onNext(true)
+        self.stopListeningMate()
         self.coreMotionService.stopPedometer()
         self.coreMotionService.stopAcitivity()
+    }
+    
+    private func checkMyDistanceSatisfy(
+        targetDistance: Double,
+        myDistance: Double
+    ) -> Bool {
+        return myDistance >= self.convertToMeter(value: targetDistance)
+    }
+    
+    private func checkTotalDistanceSatisfy(
+        targetDistance: Double,
+        myDistance: Double,
+        mateDistance: Double
+    ) -> Bool {
+        return myDistance + mateDistance >= self.convertToMeter(value: targetDistance)
     }
     
     private func updateProgress(_ progress: BehaviorSubject<Double>, value: Double) {
@@ -234,9 +281,11 @@ final class DefaultRunningUseCase: RunningUseCase {
 
 extension DefaultRunningUseCase: LocationDidUpdateDelegate {
     func locationDidUpdate(_ location: CLLocation) {
-        self.points.append(Point(
-            latitude: location.coordinate.latitude,
-            longitude: location.coordinate.longitude
-        ))
+        self.points.append(
+            Point(
+                latitude: location.coordinate.latitude,
+                longitude: location.coordinate.longitude
+            )
+        )
     }
 }
