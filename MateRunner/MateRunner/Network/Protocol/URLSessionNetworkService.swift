e8f49038d13fbb804aee1d2e9f93d67acd603ab8
Minji <966kmj@yonsei.ac.kr>
2021-11-21 01:25:04 +0900

    [Feat] URLSessionNetworkService 구현

---
+++ MateRunner/MateRunner/Network/Protocol/URLSessionNetworkService.swift
@@ -0,0 +1,14 @@
+//
+//  URLSessionNetworkService.swift
+//  MateRunner
+//
+//  Created by 김민지 on 2021/11/20.
+//
+
+import Foundation
+
+import RxSwift
+
+protocol URLSessionNetworkService {
+    func post<T: Codable>(_ data: T, url urlString: String, headers: [String: String]?) -> Observable<Void>
+}
