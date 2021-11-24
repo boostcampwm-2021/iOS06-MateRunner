b16ec80673a5da4e8d077b4158aa13474b964162
jeonyeohun <21500630@handong.edu>
2021-11-24 21:49:53 +0900

    [Refactor] URLSessionNetworkService 프로토콜 정리

--- MateRunner/MateRunner/Network/DefaultURLSessionNetworkService.swift
+++ MateRunner/MateRunner/Network/DefaultURLSessionNetworkService.swift
@@ -21,11 +21,11 @@ final class DefaultURLSessionNetworkService: URLSessionNetworkService {
         static let delete = "DELETE"
     }
     
-    func post<T: Codable>(_ data: T, url urlString: String, headers: [String: String]? = nil) -> Observable<Data> {
+    func post<T: Codable>(_ data: T, url urlString: String, headers: [String: String]?) -> Observable<Data> {
         return self.requestWithBody(data, url: urlString, headers: headers, method: HTTPMethod.post)
     }
     
-    func patch<T: Codable>(_ data: T, url urlString: String, headers: [String: String]? = nil) -> Observable<Data> {
+    func patch<T: Codable>(_ data: T, url urlString: String, headers: [String: String]?) -> Observable<Data> {
         return self.requestWithBody(data, url: urlString, headers: headers, method: HTTPMethod.patch)
     }
     
@@ -51,6 +51,38 @@ final class DefaultURLSessionNetworkService: URLSessionNetworkService {
                     observer.onNext(())
                 }
                 
+                observer.onCompleted()
+            }
+            task.resume()
+            
+            return Disposables.create {
+                task.cancel()
+            }
+        }
+    }
+    
+    func get(url urlString: String, headers: [String: String]?) -> Observable<Data> {
+        return Observable<Data>.create { observer in
+            guard let url = URL(string: urlString) else {
+                      observer.onError(URLSessionNetworkServiceError.error)
+                      observer.onCompleted()
+                      return Disposables.create()
+                  }
+            
+            var request = URLRequest(url: url)
+            request.httpMethod = HTTPMethod.get
+            
+            headers?.forEach({ header in
+                request.addValue(header.value, forHTTPHeaderField: header.key)
+            })
+            
+            let task = URLSession.shared.dataTask(with: request) { data, _, error in
+                if let error = error {
+                    observer.onError(error)
+                }
+                if let data = data {
+                    observer.onNext(data)
+                }
                 observer.onCompleted()
             }
             task.resume()
@@ -103,37 +135,5 @@ final class DefaultURLSessionNetworkService: URLSessionNetworkService {
             return data
         }
         return try? JSONEncoder().encode(requestBody)
-    }
-    
-    func get(url urlString: String, headers: [String: String]? = nil) -> Observable<Data> {
-        return Observable<Data>.create { observer in
-            guard let url = URL(string: urlString) else {
-                      observer.onError(URLSessionNetworkServiceError.error)
-                      observer.onCompleted()
-                      return Disposables.create()
-                  }
-            
-            var request = URLRequest(url: url)
-            request.httpMethod = HTTPMethod.get
-            
-            headers?.forEach({ header in
-                request.addValue(header.value, forHTTPHeaderField: header.key)
-            })
-            
-            let task = URLSession.shared.dataTask(with: request) { data, _, error in
-                if let error = error {
-                    observer.onError(error)
-                }
-                if let data = data {
-                    observer.onNext(data)
-                }
-                observer.onCompleted()
-            }
-            task.resume()
-            
-            return Disposables.create {
-                task.cancel()
-            }
-        }
     }
 }
