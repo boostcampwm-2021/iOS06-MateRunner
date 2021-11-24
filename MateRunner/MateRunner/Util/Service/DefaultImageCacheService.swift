54ecb9338e4ace91f83bb8e547c92bdb2bf60943
lee-yujinn <leeyj7988@naver.com>
2021-11-24 16:49:17 +0900

    [Feat] 캐싱 관련 코드 분리 및 이미지 서버통신 추가

--- MateRunner/MateRunner/Util/Service/DefaultImageCacheService.swift
+++ MateRunner/MateRunner/Util/Service/DefaultImageCacheService.swift
@@ -25,37 +25,65 @@ enum ImageCache {
 
 final class DefaultImageCacheService {
     func setImage(_ url: String) -> Observable<UIImage> {
-        return Observable<UIImage>.create { emitter in
+        return Observable<UIImage>.create { [weak self] emitter in
             // 1. 메모리 캐싱: 이미지가 memory cache(NSCache)에 있는지 확인
-            let cacheKey = NSString(string: url)
-            if let cachedImage = ImageCache.cache.object(forKey: cacheKey){
-                emitter.onNext(cachedImage)
+            if let image = self?.checkMemory(url) {
+                emitter.onNext(image)
             }
             
             //2. 디스크 캐싱: disk cache(UserDefault 혹은 기기Directory에있는 file형태)에서 확인
-            let fileManager = FileManager.default
-            guard let path = NSSearchPathForDirectoriesInDomains(
-                .cachesDirectory, // cache폴더를 사용
-                .userDomainMask,
-                true
-            ).first else {
-                emitter.onError(ImageCacheError.nilPathError)
+            if let image = try self?.checkDisk(url) {
+                emitter.onNext(image)
             }
             
-            var filePath = URL(fileURLWithPath: path)
-            
+            //3. 서버통신: 받은 URL로 이미지를 가져오고 캐시 저장
             if let imageURL = URL(string: url){
-                filePath.appendPathComponent(imageURL.lastPathComponent)
-                if fileManager.fileExists(atPath: filePath.path) {
-                    guard let imageData = try? Data(contentsOf: filePath),
-                          let image = UIImage(data: imageData)
-                    else {
-                        emitter.onError(ImageCacheError.nilImageError)
+                URLSession.shared.dataTask(with: imageURL, completionHandler: { (data, res, err) in
+                    if err != nil {
+                        return
                     }
-                    emitter.onNext(image)
-                }
+
+                    DispatchQueue.main.async {
+                        if let data = data, let image = UIImage(data: data) {
+                            ImageCache.cache.setObject(image, forKey: cacheKey)
+                            fileManager.createFile(atPath: filePath.path, contents: image.pngData(), attributes: nil)
+                            on(image)
+                        }
+                    }
+                }).resume()
             }
         }
     }
     
+    private func checkMemory(_ url: String) -> UIImage? {
+        // 1. 메모리 캐싱: 이미지가 memory cache(NSCache)에 있는지 확인
+        let cacheKey = NSString(string: url)
+        if let cachedImage = ImageCache.cache.object(forKey: cacheKey){
+            return cachedImage
+        }
+        return nil
+    }
+    
+    private func checkDisk(_ url: String) throws -> UIImage? {
+        //2. 디스크 캐싱: disk cache(UserDefault 혹은 기기Directory에있는 file형태)에서 확인
+        let fileManager = FileManager.default
+        guard let path = NSSearchPathForDirectoriesInDomains(
+            .cachesDirectory,
+            .userDomainMask,
+            true).first else {
+                throw ImageCacheError.nilPathError
+            }
+        var filePath = URL(fileURLWithPath: path)
+        
+        if let imageURL = URL(string: url){
+            filePath.appendPathComponent(imageURL.lastPathComponent)
+            if fileManager.fileExists(atPath: filePath.path) {
+                guard let imageData = try? Data(contentsOf: filePath),
+                      let image = UIImage(data: imageData)
+                else { return nil }
+                return image
+            }
+        }
+        return nil
+    }
 }
