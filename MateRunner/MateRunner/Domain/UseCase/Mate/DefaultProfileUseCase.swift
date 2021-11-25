867b44faeaeae8ec84d06ccbeb031d3e3c9b26d0
lee-yujinn <leeyj7988@naver.com>
2021-11-25 15:35:41 +0900

    [Feat] 메이트 프로필 화면 서버연결 Rest API로 변경

--- MateRunner/MateRunner/Domain/UseCase/Mate/DefaultProfileUseCase.swift
+++ MateRunner/MateRunner/Domain/UseCase/Mate/DefaultProfileUseCase.swift
@@ -10,37 +10,37 @@ import Foundation
 import RxSwift
 
 final class DefaultProfileUseCase: ProfileUseCase {
-    private let userRepository: UserRepository
+    private let fireStoreRepository: FirestoreRepository
     private let disposeBag = DisposeBag()
-    var userInfo: PublishSubject<UserProfileDTO> = PublishSubject()
+    var userInfo: PublishSubject<UserData> = PublishSubject()
     var recordInfo: PublishSubject<[RunningResult]> = PublishSubject()
     
-    init(repository: UserRepository) {
-        self.userRepository = repository
+    init(fireStoreRepository: FirestoreRepository) {
+        self.fireStoreRepository = fireStoreRepository
     }
     
     func fetchUserInfo(_ nickname: String) {
-        self.userRepository.fetchUserInfo(nickname)
+        self.fireStoreRepository.fetchUserData(of: nickname)
             .subscribe(onNext: { [weak self] mate in
+                guard let mate = mate else { return }
                 self?.userInfo.onNext(mate)
             })
             .disposed(by: self.disposeBag)
     }
     
     func fetchRecordList(nickname: String) {
-        self.userRepository.fetchRecordList(nickname)
-            .map { self.resultToRecordList(from: $0) }
+        self.fireStoreRepository.fetchResult(of: nickname, from: 0, by: 10)
             .subscribe(onNext: { [weak self] records in
-                self?.recordInfo.onNext(records)
+                self?.recordInfo.onNext(records ?? [])
             })
             .disposed(by: disposeBag)
     }
     
-    func resultToRecordList(from result: UserResultDTO) -> [RunningResult] {
-        var recordList: [RunningResult] = []
-        result.records.values.forEach { record in
-            recordList.append(record.toDomain())
-        }
-        return recordList
-    }
+//    func resultToRecordList(from result: UserResultDTO) -> [RunningResult] {
+//        var recordList: [RunningResult] = []
+//        result.records.values.forEach { record in
+//            recordList.append(record.toDomain())
+//        }
+//        return recordList
+//    }
 }
