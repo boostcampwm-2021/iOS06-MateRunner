5d1e59cc91b4de85407b9b2ad2950e645f6ac89d
jeonyeohun <21500630@handong.edu>
2021-11-28 18:09:32 +0900

    [Refactor] 코드정리, 함수 분리

--- MateRunner/MateRunner/Util/Service/DefaultImageCacheService.swift
+++ MateRunner/MateRunner/Util/Service/DefaultImageCacheService.swift
@@ -19,46 +19,36 @@ final class DefaultImageCacheService {
     private init () {}
     
     func setImage(_ url: String) -> Observable<UIImage> {
-        // 1. 메모리 캐싱: 이미지가 memory cache(NSCache)에 있는지 확인
+        guard let imageURL = URL(string: url) else {
+            return Observable.error(ImageCacheError.invalidURLError)
+        }
+        
+        // 1. Lookup NSCache
         if let image = self.checkMemory(url) {
-            print("mem hit")
             return Observable.just(image)
         }
-        print("mem miss")
         
-        if let image = self.checkDisk(url) {
-            print("disk hit")
-            ImageCache.cache.setObject(image, forKey: NSString(string: url))
+        // 2. Lookup Disk
+        if let image = self.checkDisk(imageURL) {
+            self.saveIntoCache(imageURL: imageURL, image: image)
             return Observable.just(image)
         }
-        print("disk miss")
         
-        guard let imageURL = URL(string: url) else { return Observable.error(ImageCacheError.nilImageError) }
+        // 3. Network Request
         return Observable<UIImage>.create { emitter in
-            // 3. 서버통신: 받은 URL로 이미지를 가져오고 캐시 저장
-            URLSession.shared.dataTask(with: imageURL, completionHandler: { (data, _, error) in
-                if error != nil {
-                    emitter.onError(ImageCacheError.nilImageError)
-                    return
-                }
-                if let data = data, let image = UIImage(data: data) {
-                    guard let path = NSSearchPathForDirectoriesInDomains( .cachesDirectory, .userDomainMask, true).first,
-                          let tokenString  = url.components(separatedBy: "=").last,
-                          let imageURL = URL(string: url) else {
-                              return
-                          }
-                    
-                    let filePath = URL(fileURLWithPath: path).appendingPathComponent(tokenString + imageURL.path)
-                    ImageCache.cache.setObject(image, forKey: NSString(string: url))
-                    print("save cache for \(url)")
-                    FileManager.default.createFile(atPath: filePath.path, contents: image.pngData(), attributes: nil)
-                    print("save distk for \(filePath.path)")
-                    print(ImageCache.cache)
-                    print("net cache")
+            let request = URLRequest(url: imageURL)
+            URLSession.shared.rx.response(request: request).subscribe(
+                onNext: { [weak self] response in
+                    guard let image = UIImage(data: response.data) else { return }
+                    self?.saveIntoCache(imageURL: imageURL, image: image)
+                    self?.saveIntoDisk(imageURL: imageURL, image: image)
                     emitter.onNext(image)
+                },
+                onError: { error in
+                    emitter.onError(error)
                 }
-            }).resume()
-            
+            ).disposed(by: self.disposeBag)
+
             return Disposables.create()
         }
     }
@@ -71,47 +61,30 @@ final class DefaultImageCacheService {
         return nil
     }
     
-    private func checkDisk(_ url: String) -> UIImage? {
-        // 2. 디스크 캐싱: disk cache(UserDefault 혹은 기기Directory에있는 file형태)에서 확인
-        let fileManager = FileManager.default
-        guard let path = NSSearchPathForDirectoriesInDomains( .cachesDirectory, .userDomainMask, true).first,
-              let tokenString  = url.components(separatedBy: "=").last,
-              let imageURL = URL(string: url) else {
-                  return nil
-              }
+    private func checkDisk(_ imageURL: URL) -> UIImage? {
+        guard let path = NSSearchPathForDirectoriesInDomains(
+            .cachesDirectory,
+            .userDomainMask,
+            true
+        ).first else { return nil }
         
-        let filePath = URL(fileURLWithPath: path).appendingPathComponent(tokenString + imageURL.path)
-        print("filePath", filePath)
-        if fileManager.fileExists(atPath: filePath.path) {
-            guard let imageData = try? Data(contentsOf: filePath),
-                  let image = UIImage(data: imageData)
-            else { return nil }
-            return image
+        let filePath = URL(fileURLWithPath: path).appendingPathComponent(imageURL.path)
+        if FileManager.default.fileExists(atPath: filePath.path) {
+            guard let imageData = try? Data(contentsOf: filePath) else { return nil }
+            return UIImage(data: imageData)
         }
         return nil
     }
     
-//    private func createImageCachePath(with url: String) {
-//        guard let path = NSSearchPathForDirectoriesInDomains( .cachesDirectory, .userDomainMask, true).first,
-//              let tokenString  = url.components(separatedBy: "=").last,
-//              let imageURL = URL(string: url) else {
-//                  return nil
-//              }
-//    }
+    private func saveIntoCache(imageURL: URL, image: UIImage) {
+        ImageCache.cache.setObject(image, forKey: NSString(string: imageURL.path))
+    }
     
-    // 2. 디스크 캐싱: disk cache(UserDefault 혹은 기기Directory에있는 file형태)에서 확인
-    //      let fileManager = FileManager.default
-    //      let path = NSSearchPathForDirectoriesInDomains(
-    //        .cachesDirectory,
-    //        .userDomainMask,
-    //        true).first ?? “”
-    //      var filePath = URL(fileURLWithPath: path)
-    //      if let imageURL = URL(string: url) {
-    //        filePath.appendPathComponent(imageURL.lastPathComponent)
-    //        if let image = self?.checkDisk(filePath: filePath, url: imageURL) {
-    //          ImageCache.cache.setObject(image, forKey: NSString(string: url))
-    //          emitter.onNext(image)
-    //          emitter.onCompleted()
-    //        }
-    //      }
+    private func saveIntoDisk(imageURL: URL, image: UIImage) {
+        guard let path = NSSearchPathForDirectoriesInDomains( .cachesDirectory, .userDomainMask, true).first else {
+            return
+        }
+        let filePath = URL(fileURLWithPath: path).appendingPathComponent(imageURL.path)
+        FileManager.default.createFile(atPath: filePath.path, contents: image.pngData(), attributes: nil)
+    }
 }
