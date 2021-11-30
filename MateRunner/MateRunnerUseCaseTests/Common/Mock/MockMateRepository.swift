01fa0aefcc06a266034723dd8e471253eedf23bf
lee-yujinn <leeyj7988@naver.com>
2021-12-01 03:56:23 +0900

    [Feat] MockMateRepository 추가

--- MateRunner/MateRunnerUseCaseTests/Common/Mock/MockMateRepository.swift
+++ MateRunner/MateRunnerUseCaseTests/Common/Mock/MockMateRepository.swift
@@ -1,8 +1,24 @@
 //
-//  MockMateRepository.swift
-//  MateRunnerUseCaseTests
-//
-//  Created by 이유진 on 2021/11/30.
-//
+ //  MockMateRepository.swift
+ //  MateRunnerViewModelTests
+ //
+ //  Created by 이유진 on 2021/11/30.
+ //
 
-import Foundation
+ import Foundation
+
+ import RxSwift
+
+ final class MockMateRepository: MateRepository {
+     func sendRequestMate(from sender: String, fcmToken: String) -> Observable<Void> {
+         return Observable.just(())
+     }
+
+     func fetchFCMToken(of mate: String) -> Observable<String> {
+         return Observable.just("2341asdgf1ddf")
+     }
+
+     func sendEmoji(from sender: String, fcmToken: String) -> Observable<Void> {
+         return Observable.just(())
+     }
+ }
