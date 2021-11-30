bb4140289ade890d4d01fc1c30b081bdfb748d78
jeonyeohun <21500630@handong.edu>
2021-11-30 22:11:52 +0900

    [Chore] 사용하지 않는 코드 제거

--- MateRunner/MateRunner/Presentation/LoginScene/ViewController/SignUpViewController.swift
+++ MateRunner/MateRunner/Presentation/LoginScene/ViewController/SignUpViewController.swift
@@ -274,12 +274,4 @@ private extension SignUpViewController {
         stackView.addArrangedSubview(self.descriptionLabel)
         return stackView
     }
-    
-    func randomEmoji() -> String {
-        let range = 0x1F300...0x1F3F0
-        let index = Int(arc4random_uniform(UInt32(range.count)))
-        let ord = range.lowerBound + index
-        guard let scalar = UnicodeScalar(ord) else { return "❓" }
-        return String(scalar)
-    }
 }
