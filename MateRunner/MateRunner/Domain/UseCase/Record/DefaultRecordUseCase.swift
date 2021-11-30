24c901505709f23b3d22701e541084969281423b
jungwonlee <dev.jungwon.lee@gmail.com>
2021-11-30 20:20:46 +0900

    [Fix] 기록없는 달 기록 개수, 이모지 개수 표시 안되는 버그 해결

--- MateRunner/MateRunner/Domain/UseCase/Record/DefaultRecordUseCase.swift
+++ MateRunner/MateRunner/Domain/UseCase/Record/DefaultRecordUseCase.swift
@@ -49,22 +49,23 @@ final class DefaultRecordUseCase: RecordUseCase {
         
         self.firestoreRepository.fetchResult(of: nickname, from: currentMonth, to: nextMonth)
             .subscribe(onNext: { [weak self] records in
-                Observable<RunningResult>.zip(records.map { [weak self] record in
-                    self?.fetchRecordEmoji(record, from: nickname) ?? Observable.of(record)
-                })
-                    .subscribe(onNext: { [weak self] records in
-                        guard let self = self else { return }
-                        self.monthlyRecords.onNext(records)
-                        self.selectedDay.onNext(try? self.selectedDay.value())
-                        self.runningCount.onNext(records.count)
-                        self.likeCount.onNext(self.getLikeCount(from: records))
+                if records.isEmpty {
+                    self?.monthlyRecords.onNext([])
+                    self?.runningCount.onNext(0)
+                    self?.likeCount.onNext(0)
+                } else {
+                    Observable<RunningResult>.zip(records.map { [weak self] record in
+                        self?.fetchRecordEmoji(record, from: nickname) ?? Observable.of(record)
                     })
-                    .disposed(by: self?.disposeBag ?? DisposeBag())
-                
-            }, onError: { [weak self] _ in
-                self?.monthlyRecords.onNext([])
-                self?.runningCount.onNext(0)
-                self?.likeCount.onNext(0)
+                        .subscribe(onNext: { [weak self] records in
+                            guard let self = self else { return }
+                            self.monthlyRecords.onNext(records)
+                            self.selectedDay.onNext(try? self.selectedDay.value())
+                            self.runningCount.onNext(records.count)
+                            self.likeCount.onNext(self.getLikeCount(from: records))
+                        })
+                        .disposed(by: self?.disposeBag ?? DisposeBag())
+                }
             })
             .disposed(by: self.disposeBag)
     }
