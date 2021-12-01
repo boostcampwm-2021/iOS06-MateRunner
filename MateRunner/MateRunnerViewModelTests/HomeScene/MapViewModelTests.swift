c465e8fb3c93afbd46fc77f23f528264d02bc3e2
jeonyeohun <21500630@handong.edu>
2021-12-01 17:27:15 +0900

    [Feat] MapViewModel 테스트 추가

--- MateRunner/MateRunnerViewModelTests/HomeScene/MapViewModelTests.swift
+++ MateRunner/MateRunnerViewModelTests/HomeScene/MapViewModelTests.swift
@@ -29,7 +29,7 @@ class MapViewModelTests: XCTestCase {
         self.disposeBag = DisposeBag()
     }
     
-    func test_loacte_button_did_tap_should_set_center_true() {
+    func test_loacte_button_did_tap_trigger_should_set_center_true() {
         let viewDidAppearTestableObservable = self.scheduler.createHotObservable([
             .next(10, ())
         ])
@@ -53,6 +53,7 @@ class MapViewModelTests: XCTestCase {
         
         self.viewModel.transform(input: self.input, disposeBag: self.disposeBag)
             .shouldSetCenter
+            .skip(3)
             .subscribe(testableObserver)
             .disposed(by: self.disposeBag)
         
@@ -63,16 +64,72 @@ class MapViewModelTests: XCTestCase {
         ])
     }
     
-//    struct Input {
-//        let viewDidAppearEvent: Observable<Void>
-//        let locateButtonDidTapEvent: Observable<Void>
-//        let backButtonDidTapEvent: Observable<Void>
-//        let mapDidPanEvent: Observable<Void>
-//    }
-//
-//    struct Output {
-//        let shouldSetCenter: BehaviorRelay<Bool> = BehaviorRelay(value: true)
-//        let shouldMoveToFirstPage: PublishRelay<Bool> = PublishRelay()
-//        let coordinatesToDraw: PublishRelay<(CLLocationCoordinate2D, CLLocationCoordinate2D)> = PublishRelay()
-//    }
+    func test_map_did_pan_trigger_should_set_center_false() {
+        let viewDidAppearTestableObservable = self.scheduler.createHotObservable([
+            .next(10, ())
+        ])
+        let backButtonTestableObservable = self.scheduler.createHotObservable([
+            .next(20, ())
+        ])
+        let locateButtonTestableObservable = self.scheduler.createHotObservable([
+            .next(30, ())
+        ])
+        let mapDidPanTestableObservable = self.scheduler.createHotObservable([
+            .next(40, ())
+        ])
+        let testableObserver = self.scheduler.createObserver(Bool.self)
+        
+        self.input = MapViewModel.Input(
+            viewDidAppearEvent: viewDidAppearTestableObservable.asObservable(),
+            locateButtonDidTapEvent: locateButtonTestableObservable.asObservable(),
+            backButtonDidTapEvent: backButtonTestableObservable.asObservable(),
+            mapDidPanEvent: mapDidPanTestableObservable.asObservable()
+        )
+        
+        self.viewModel.transform(input: self.input, disposeBag: self.disposeBag)
+            .shouldSetCenter
+            .skip(3)
+            .subscribe(testableObserver)
+            .disposed(by: self.disposeBag)
+        
+        scheduler.start()
+        
+        XCTAssertEqual(testableObserver.events, [
+            .next(40, false)
+        ])
+    }
+    
+    func test_back_button_did_tap_trigger_should_move_first_page_true() {
+        let viewDidAppearTestableObservable = self.scheduler.createHotObservable([
+            .next(10, ())
+        ])
+        let backButtonTestableObservable = self.scheduler.createHotObservable([
+            .next(40, ())
+        ])
+        let locateButtonTestableObservable = self.scheduler.createHotObservable([
+            .next(20, ())
+        ])
+        let mapDidPanTestableObservable = self.scheduler.createHotObservable([
+            .next(30, ())
+        ])
+        let testableObserver = self.scheduler.createObserver(Bool.self)
+        
+        self.input = MapViewModel.Input(
+            viewDidAppearEvent: viewDidAppearTestableObservable.asObservable(),
+            locateButtonDidTapEvent: locateButtonTestableObservable.asObservable(),
+            backButtonDidTapEvent: backButtonTestableObservable.asObservable(),
+            mapDidPanEvent: mapDidPanTestableObservable.asObservable()
+        )
+        
+        self.viewModel.transform(input: self.input, disposeBag: self.disposeBag)
+            .shouldMoveToFirstPage
+            .subscribe(testableObserver)
+            .disposed(by: self.disposeBag)
+        
+        scheduler.start()
+        
+        XCTAssertEqual(testableObserver.events, [
+            .next(40, true)
+        ])
+    }
 }
