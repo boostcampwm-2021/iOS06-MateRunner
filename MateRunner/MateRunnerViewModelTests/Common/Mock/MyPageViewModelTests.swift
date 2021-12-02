a7863ff5766105e6f57a31fd18197f8d2d9517d0
lee-yujinn <leeyj7988@naver.com>
2021-12-02 17:03:01 +0900

    [Feat] MockMyPageUseCase 추가

---
+++ MateRunner/MateRunnerViewModelTests/Common/Mock/MyPageViewModelTests.swift
@@ -0,0 +1,32 @@
+//
+//  MyPageViewModelTests.swift
+//  MateRunnerViewModelTests
+//
+//  Created by 이유진 on 2021/12/02.
+//
+
+import XCTest
+
+class MyPageViewModelTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
