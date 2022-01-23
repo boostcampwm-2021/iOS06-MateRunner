c5ad3d34a99fe2e5a2aaba3b62427ca85bcae661
jeonyeohun <21500630@handong.edu>
2022-01-23 15:51:27 +0900

    [Feat] 캐시 정책 사이즈 상수 파일 생성

---
+++ MateRunner/MateRunner/Util/Constant/CacheSizeConstants.swift
@@ -0,0 +1,13 @@
+//
+//  CacheSizeConstants.swift
+//  MateRunner
+//
+//  Created by 전여훈 on 2022/01/23.
+//
+
+import Foundation
+
+enum CacheConstants {
+    static let maximumMemoryCacheSize = 52428800
+    static let maximumDiskCacheSize = 52428800
+}
